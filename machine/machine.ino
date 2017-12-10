8f121f4b127d2c17d05e1f6e53227b082ee14256
Torsten Martinsen <torsten@bullestock.net>
2017-12-10 11:07:20 +0100

    Move card handling to class.

--- machine/machine.ino
+++ machine/machine.ino
@@ -1,8 +1,8 @@
 #define SERIAL_DBG  0
 
-#include <RDM6300.h>
-#include <SoftwareSerial.h>
+#include <RestClient.h>
 
+#include "cardreader.h"
 #include "display.h"
 #include "eeprom.h"
 #include "led.h"
@@ -17,7 +17,6 @@ const char* VERSION = "0.0.1";
 #define PIN_LED      13
 #define PIN_RELAY    15
 
-SoftwareSerial swSerial(PIN_RX, PIN_TX);
 
 Display display;
 
@@ -34,7 +33,6 @@ void setup()
     Serial.begin(115200);
     Serial.print("Cardreader v ");
     Serial.println(VERSION);
-    swSerial.begin(9600);
 
     display.set_machine_id(Eeprom::get_machine_id());
     String s = "Version ";
@@ -45,58 +43,31 @@ void setup()
     wifi_handler.init(rgb_led, display);
 }
 
-RDM6300 decoder;
-int n = 0;
-bool card_present = false;
-String current_id;
-unsigned long last_sent_millis = 0;
+CardReader reader(PIN_RX, PIN_TX, PIN_SWITCH);
+String last_card_id;
 
 void loop()
 {
-    const bool switch_on = digitalRead(PIN_SWITCH);
-    const auto c = swSerial.read();
-    if (c > 0)
+    reader.update();
+    
+    const auto card_id = reader.get_card_id();
+    if (card_id != last_card_id)
     {
-        if (decoder.add_byte(c))
+        last_card_id = card_id;
+        if (card_id.length())
         {
-            const auto id = decoder.get_id();
-#if SERIAL_DBG
-            Serial.print("ID: ");
-            Serial.println(id);
-#endif
-            if (switch_on)
-            {
-                current_id = id;
-#if SERIAL_DBG
-                Serial.println("Saved");
-#endif
-                display.set_status("Card present");
-            }
-#if SERIAL_DBG
-            else
-            {
-                Serial.println("Ignored - switch off");
-            }
-#endif
-        }
-    }
-    const auto now = millis();
-    const auto since_last_send = now - last_sent_millis;
-    if (since_last_send > 100)
-    {
-        last_sent_millis = now;
-        if (switch_on && current_id.length())
-        {
-            Serial.print("Card: ");
-            Serial.println(current_id);
+            display.set_status("Card present");
+            RestClient client("192.168.0.", 5000);
+    
         }
         else
         {
-            current_id = "";
             if (!showing_version)
                 display.set_status("No card");
         }
     }
+
+    const auto now = millis();
     if (now - start_tick > 5000)
         showing_version = false;
 
