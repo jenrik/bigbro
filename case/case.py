c4b0d5c21aec5432c335d7e5bab888d25f95b4a6
Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
2018-08-23 14:19:17 +0200

    moved firmware to subfolder, and added case-files back

---
+++ case/case.py
@@ -0,0 +1,82 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import division
+import os
+import sys
+import re
+
+# Assumes SolidPython is in site-packages or elsewhwere in sys.path
+from solid import *
+from solid.utils import *
+
+SEGMENTS = 16
+
+card_pocket_w = 56
+card_pocket_h = 50
+
+case_w = card_pocket_w+5
+case_h = card_pocket_h+5
+case_d = 25
+case_th = 2
+coil_sup_l = 3
+coil_sup_d = 3
+# Inner diameter of coil
+coil_sup_w = 43
+coil_sup_h = 31
+
+def coil_sup():
+    return cylinder(coil_sup_d/2, coil_sup_l)
+
+def coil_sups():
+    s1 = translate([-(coil_sup_w-coil_sup_d)/2, -(coil_sup_h-coil_sup_d)/2, 0])(coil_sup())
+    s2 = translate([(coil_sup_w-coil_sup_d)/2, -(coil_sup_h-coil_sup_d)/2, 0])(coil_sup())
+    s3 = translate([(coil_sup_w-coil_sup_d)/2, (coil_sup_h-coil_sup_d)/2, 0])(coil_sup())
+    s4 = translate([-(coil_sup_w-coil_sup_d)/2, (coil_sup_h-coil_sup_d*2)/2, 0])(coil_sup())
+    return s1+s2+s3+s4
+
+def bottom():
+    return translate([-case_w/2, -case_h/2, 0])(cube([case_w, case_h, case_th]))
+
+def frame():
+    outer = translate([-case_w/2, -case_h/2, case_th])(cube([case_w, case_h, case_d]))
+    iw = case_w-2*case_th
+    ih = case_h-2*case_th
+    inner = translate([-iw/2, -ih/2, case_th])(cube([iw, ih, case_d+1]))
+    return outer-inner
+
+def screw_support():
+    return cylinder(5, 4)
+
+def screw_hole():
+    return cylinder(2, 6) + cylinder(2, 3, 4)
+
+switch_offset = -22
+
+def switch_hole():
+    flap_hole = cube([5, 18, 5])
+    sw_w = 6.25
+    return translate([-sw_w/2, switch_offset, -0.5])(cube([sw_w, 24, 10]) +
+                                    translate([.5, 3, -4.5])(flap_hole))
+
+def switch_mount():
+    return translate([-5, switch_offset+5, 1])(cube([10, 15, 8]))
+
+def screw_hole():
+    return up(case_d-5)(left(5)(rotate([90, 0, 90])(cylinder(r=1.5, h = 10))))
+
+def assembly():
+    bt = bottom()
+    cs = up(case_th)(forward(switch_offset/2+5)(coil_sups()))
+    fr = frame()
+
+    swm = switch_mount()
+    swh = switch_hole()
+    sh1 = translate([-case_w/2, -15, 0])(screw_hole())
+    sh2 = translate([-case_w/2, 15, 0])(screw_hole())
+    sh3 = translate([case_w/2, -15, 0])(screw_hole())
+    sh4 = translate([case_w/2, 15, 0])(screw_hole())
+    return cs+fr+bt + translate([0, 0, 0])(swm) - translate([0, 0, 1])(swh) - sh1 - sh2 - sh3 - sh4
+
+if __name__ == '__main__':
+    a = assembly()
+    scad_render_to_file(a, file_header='$fn = %s;' % SEGMENTS, include_orig_code=False)
---
+++ case/lid.py
@@ -0,0 +1,75 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import division
+import os
+import sys
+import re
+
+# Assumes SolidPython is in site-packages or elsewhwere in sys.path
+from solid import *
+from solid.utils import *
+
+SEGMENTS = 16
+
+card_pocket_w = 56
+card_pocket_h = 50
+
+case_w = card_pocket_w+5
+case_h = card_pocket_h+5
+case_th = 2
+
+epsilon = 0.5
+
+def lid_top():
+    return cube([case_w, case_h, case_th], center=True)
+
+def lid_bottom():
+    return cube([case_w-2*case_th-epsilon, case_h-2*case_th-epsilon, case_th], center=True)
+
+def post():
+    post_w = 6
+    post_h = 6
+    pcb_h = 2
+    nub_d = 0.5
+    offset = 1
+    p = translate([-post_w/2, -post_w/2, 0])(cube([post_w, post_w, post_h+offset]))
+    slot = translate([0, 0, pcb_h])(cube([post_w, post_w, post_h]))
+    nub = translate([0, post_w/2, 2*pcb_h])(rotate([90, 0, 0])(cylinder(nub_d/2, post_w/2)))
+    return p-slot+nub
+
+def rfid_sups():
+    return pcb_sup(18.5, 39.5)
+
+def ardu_sups():
+    return pcb_sup(18.5, 44)
+
+def pcb_sup(w, l):
+    s1 = translate([-w/2, -l/2, 0])(post())
+    s2 = translate([w/2, -l/2, 0])(mirror([1, 0, 0])(post()))
+    s3 = translate([-w/2, l/2, 0])(mirror([0, 0, 0])(rotate([0, 0, 270])(post())))
+    s4 = translate([w/2, l/2, 0])(mirror([1, 0, 0])(rotate([0, 0, 270])(post())))
+    return s1+s2+s3+s4
+
+def side():
+    h = case_h-(case_th+epsilon)*2
+    return translate([0, -h/2, case_th-epsilon])(cube([2, h, 20]))
+
+def ring():
+    w = case_w
+    h = case_h
+    w2 = w - case_th*2
+    h2 = h - case_th*2
+    return translate([-w/2, -h/2, 0])(cube([w, h, 10])) - translate([-w2/2, -h2/2, -1])(cube([w2, h2, 12]))
+    
+def assembly():
+    lt = lid_top()
+    lb = up(case_th)(lid_bottom())
+    rfs = rotate([0, 0, 90])(translate([case_w/4, 0, case_th*2-1])(rfid_sups()))
+    r4s = rotate([0, 0, 90])(translate([-case_w/4, 0, case_th*2-1])(ardu_sups()))
+    s1 = translate([-case_w/2+case_th+epsilon/2, 0, 0])(side())
+    s2 = translate([case_w/2-case_th-epsilon/2-2, 0, 0])(side())
+    return lt+lb+rfs+r4s+s1+s2+ring()
+
+if __name__ == '__main__':
+    a = assembly()
+    scad_render_to_file(a, file_header='$fn = %s;' % SEGMENTS, include_orig_code=False)
